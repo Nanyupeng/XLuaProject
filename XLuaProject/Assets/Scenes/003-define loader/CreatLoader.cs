0203ad01af364f73b8a1ccb0c1afa458c4f076f3
Nan Yupeng <NanYuPeng@DanDan.PiPi.github.com>
2020-03-10 17:54:49 +0800

    Xlua自定义loader启动器

--- XLuaProject/Assets/Scenes/003-define loader/CreatLoader.cs
+++ XLuaProject/Assets/Scenes/003-define loader/CreatLoader.cs
@@ -1,8 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using XLua;
 public class CreatLoader : MonoBehaviour
 {
-     
+    LuaEnv luaEnv;
+    private void Start()
+    {
+        luaEnv = new LuaEnv();
+        //luaEnv.DoString("require 'helloworld'");
+        luaEnv.DoString("require 'helloworld'");
+        luaEnv.AddLoader(MyLoader);
+
+        luaEnv.Dispose();
+    }
+
+    byte[] MyLoader(ref string filePath)
+    {
+        print(filePath);
+        string s = "print(10+20)";
+        return System.Text.Encoding.UTF8.GetBytes(s);
+    }
 }
