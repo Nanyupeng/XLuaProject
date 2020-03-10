fabb7bbd22d46b514814863d1e65df94c3d64ce0
Nan Yupeng <NanYuPeng@DanDan.PiPi.github.com>
2020-03-10 15:24:05 +0800

    第一次上传资源
    1. Xlua的导入
    2.Xlua与C#的代码基础交互

---
+++ XLuaProject/Assets/Scenes/001-helloworld/helloworld01.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+public class helloworld01 : MonoBehaviour
+{
+    private LuaEnv luaEnv;
+    private void Start()
+    {
+        luaEnv = new LuaEnv();
+        luaEnv.DoString("print('helloworld!')");
+    }
+
+    private void OnDestroy()
+    {
+        luaEnv.Dispose();
+    }
+}
---
+++ XLuaProject/Assets/Scenes/002-helloworld by file/helloworld02.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+public class helloworld02 : MonoBehaviour
+{
+    private LuaEnv luaEnv;
+    private void Start()
+    {
+        luaEnv = new LuaEnv();
+        //第一种方式
+        //TextAsset textAsset = Resources.Load<TextAsset>("helloworld.lua");
+        //luaEnv.DoString(textAsset.text); //DOString 用于执行lua方法的函数
+        //第二种方式
+        luaEnv.DoString("require 'helloworld'");//使用lua中的require关键字 搜索helloworld脚本 并执行里面的方法
+        luaEnv.Dispose();
+    }
+}
