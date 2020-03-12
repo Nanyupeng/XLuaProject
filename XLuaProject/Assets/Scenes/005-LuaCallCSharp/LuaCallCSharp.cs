e41a2548f9b9d2f29a5dc2a140f7de856f96a13c
Nan Yupeng <NanYuPeng@DanDan.PiPi.github.com>
2020-03-12 17:01:47 +0800

    LuaCallCSharp

---
+++ XLuaProject/Assets/Scenes/005-LuaCallCSharp/LuaCallCSharp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+public class LuaCallCSharp : MonoBehaviour
+{
+    LuaEnv luaEnv;
+    private void Awake()
+    {
+        luaEnv = new LuaEnv();
+        luaEnv.DoString("require 'LuaCallCSharp'");
+    }
+
+    private void OnDestroy()
+    {
+        luaEnv.Dispose();
+    }
+}
