bdb0f9457a52ee04bb6acc1ed69c4e24cbe097d8
Nan Yupeng <NanYuPeng@DanDan.PiPi.github.com>
2020-03-12 12:35:35 +0800

    C#访问Lua数据与Function
    访问数据建议使用interface映射。
    访问Function建议使用delegate映射，在程序中不要多次调用，可以在Awake中映射一次。

--- XLuaProject/Assets/Scenes/004-CSharpCallLua/CSharpCallLua.cs
+++ XLuaProject/Assets/Scenes/004-CSharpCallLua/CSharpCallLua.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using XLua;
 using LitJson;
+using System;
+
 public class CSharpCallLua : MonoBehaviour
 {
+    LuaEnv luaEnv;
+    [CSharpCallLua]
+    delegate void Add1(int a, int b);
+    [CSharpCallLua]
+    delegate int Add2(int a, int b);
+    [CSharpCallLua]
+    delegate int Add3(int a, int b, out int c, out int d);
     private void Start()
     {
-        LuaEnv luaEnv = new LuaEnv();
+        luaEnv = new LuaEnv();
         luaEnv.DoString("require 'CSharpCallLua'");
 
         //基础数据类型获取方式
@@ -52,15 +61,49 @@ public class CSharpCallLua : MonoBehaviour
         //    Debug.Log(o);
 
         //LuaTable类获取XluaTable数据
-        LuaTable luaTable = luaEnv.Global.Get<LuaTable>("person");
-        print(luaTable.Get<string>("name"));
-        print(luaTable.Get<string>("age"));
+        //LuaTable luaTable = luaEnv.Global.Get<LuaTable>("person");
+        //print(luaTable.Get<string>("name"));
+        //print(luaTable.Get<string>("age"));
         //foreach (string item in luaTable.GetKeys())
         //{
         //    print(luaTable.GetInPath<string>(item));
         //}
 
+        ////调用Lua中的函数  --映射到delegate
+        //Action act = luaEnv.Global.Get<Action>("add");
+        //act();
+        //act = null;
+        ////调用Lua中的函数 并且加入参数   --映射到delegate
+        //Add1 act1 = luaEnv.Global.GetInPath<Add1>("add1");
+        //act1(54, 65);
+        //act1 = null;
+        ////调用Lua中的函数 加入参数 并且有返回值   --映射到delegate
+        //Add2 act2 = luaEnv.Global.GetInPath<Add2>("add2");
+        //int sum = act2(54, 54);
+        //Debug.Log(sum);
+        //act2 = null;
+        ////调用Lua中的函数 加入参数 并且有多个返回值    --映射到delegate
+        //Add3 act3 = luaEnv.Global.GetInPath<Add3>("add3");
+        //int sum1, a, b;
+        //sum1 = act3(25, 89, out a, out b);
+        //Debug.Log(sum1 + "---" + a + "---" + b);
+        //act3 = null;
 
+
+        //映射到LuaFunction、
+        LuaFunction luaFunction = luaEnv.Global.GetInPath<LuaFunction>("add");
+        luaFunction.Call();
+        luaFunction = null;
+        //映射到LuaFunction 带有参数与返回值
+        LuaFunction luaFunction1 = luaEnv.Global.GetInPath<LuaFunction>("add3");
+        object[] o = luaFunction1.Call(45,45);
+        foreach (object index in o)
+            print(index);
+        luaFunction1 = null;
+    }
+
+    private void OnDestroy()
+    {
         luaEnv.Dispose();
     }
 }
